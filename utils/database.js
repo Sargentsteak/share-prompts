fb2eec521d58e53555910a3470e748c04f721ba3
Sargentsteak <piyushjsb@gmail.com>
2024-03-31 01:40:59 +0530

    commit

--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+require("dotenv").config();
 
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        isConnected = true; // Correct assignment here
-        console.log("mongodb connected")
-    } catch (error) {
-        console.log(error);
-    }
-}
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true; // Correct assignment here
+    console.log("mongodb connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
