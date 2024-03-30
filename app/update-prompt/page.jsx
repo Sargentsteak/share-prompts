89539a085c6625e57cc6be1e0e6495b5b401792b
Sargentsteak <piyushjsb@gmail.com>
2024-03-31 02:00:52 +0530

    commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,26 +1,28 @@
 "use client";
+import { useEffect, useState, Suspense } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
-
 import Form from "@components/Form";
 
-const EditPrompt = () => {
-  const Router = useRouter();
+const UpdatePrompt = () => {
+  const router = useRouter();
+
   const { data: session } = useSession();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
 
   const [submitting, setSubmitting] = useState(false);
+
+  const searchParams = useSearchParams();
+
+  const promptId = searchParams.get("id");
+
   const [post, setPost] = useState({
-    prompt: " ",
+    prompt: "",
     tag: "",
   });
 
   useEffect(() => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
-
       const data = await response.json();
 
       setPost({
@@ -28,14 +30,18 @@ const EditPrompt = () => {
         tag: data.tag,
       });
     };
-    if (promptId) getPromptDetails();
+    if (promptId) {
+      getPromptDetails();
+    }
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if (!promptId) return alert("Prompt ID not found");
+    if (!promptId) {
+      return alert("Prompt ID  not found!");
+    }
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -47,7 +53,7 @@ const EditPrompt = () => {
       });
 
       if (response.ok) {
-        Router.push("/");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
@@ -63,7 +69,15 @@ const EditPrompt = () => {
       setPost={setPost}
       submitting={submitting}
       handleSubmit={updatePrompt}
-    />
+    ></Form>
+  );
+};
+
+const EditPrompt = () => {
+  return (
+    <Suspense>
+      <UpdatePrompt />
+    </Suspense>
   );
 };
 
